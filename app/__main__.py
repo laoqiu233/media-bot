342671e162a8ad3ae7981d648c1bf181e6db09c8
e.vereshchagin <e.vereshchagin@vkteam.ru>
2025-11-10 15:48:52 +0300

    add bot

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,7 @@
 """Entry point for running app as a module with python -m app."""
 
-from app.main import main
 from app.bot import run_bot
 
 if __name__ == "__main__":
     run_bot()
-    main()
 
